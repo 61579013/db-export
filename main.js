56f329c83aac92ff717f509d5a00fa4af94076c3
gouguoyin <245629560@qq.com>
2020-08-06 23:27:41 +0800

    int

--- main.js
+++ main.js
@@ -1,47 +1,47 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const electron = require('electron');
+const path = require('path');
+const app  = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+let mainWindow;
 
 function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  //创建一个 800x600 的浏览器窗口
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     // transparent: true, // 窗口透明
     // frame: false, // 边框隐藏
+    // fullscreen: false,  // 不允许全屏
     // titleBarStyle: 'hidden', // 隐藏MAC标题
     resizable: false, // 固定宽高
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
-  })
+  });
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  //加载应用的界面文件
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  //打开开发者工具，方便调试
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
+app.on('ready', function () {
+  createWindow();
+});
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
