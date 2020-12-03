dc5f7b047815d435aac7b69be446c1af65682775
gouguoyin <245629560@qq.com>
2020-12-03 18:31:08 +0800

    文件上传

--- download/js/download.js
+++ download/js/download.js
@@ -3,8 +3,8 @@ const { dialog, app } = require('electron').remote
 async function openUploadDialog(){
   const res = dialog.showOpenDialogSync({
     title: '请选择csv文件',
-    // 默认打开的路径，比如这里默认打开下载文件夹
-    defaultPath: app.getPath('downloads'),
+    // 默认打开的路径
+    defaultPath: app.getPath('desktop'),
     buttonLabel: '确认',
     // 限制能够选择的文件类型
     filters: [
